d8ac4948d33ecd22a2fd26e5a12ff7e522bcdcf1
AdiDD <adi.deaconu21@gmail.com>
2021-04-06 20:31:30 +0300

    Import only start_screen and get_answer from view

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import view
+from view import start_screen, get_answer
 from one_min_quiz import one_min_quiz
 from basic_quiz import basic_quiz
 
 
 def main():
-    view.start_screen()
-    answer = view.get_answer("Make your choice: ")
+    start_screen()
+    answer = get_answer("Make your choice: ")
     while answer not in ["1", "2"]:
-        answer = view.get_answer("Pleas enter 1 or 2: ")
+        answer = get_answer("Pleas enter 1 or 2: ")
     if answer == "1":
         basic_quiz()
     else:
