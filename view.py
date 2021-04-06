1bd91204253d6d876c3ade7044baf6dc33f7cf10
AdiDD <adi.deaconu21@gmail.com>
2021-04-06 20:19:06 +0300

    Modified message for start_screen and refactored print_finish_screen

--- view.py
+++ view.py
@@ -1,6 +1,6 @@
 def start_screen():
     print("Welcome to the progbasiz quiz!")
-    print("If you want to take a basic quiz (25 questions in 20 minutes), press 1")
+    print("If you want to take a basic quiz (25 questions in 25 minutes), press 1")
     print("If you want to test how many questions you can answer in 1 minute, press 2")
 
 
@@ -20,15 +20,15 @@ def print_stats(questions_answered, mistakes, limit=0):
     print("\n--------------------------------------------------\n")
 
 
-def print_finish_screen(questions_answered, mistakes, passed=True, limit=0):
+def print_finish_screen(questions_answered_correctly, passed=True, limit=0):
     if passed and limit > 0:
-        print("You passed!")
-        print(f"Questions answered: {questions_answered}/{limit}, Mistakes: {mistakes}")
+        print("Congratulations, you passed!")
+        print(f"You answered {questions_answered_correctly} questions correctly out of {limit}.")
     elif not passed and limit > 0:
         print("You failed!")
-        print(f"Questions answered: {questions_answered}/{limit}, Mistakes: {mistakes}")
+        print(f"You answered {questions_answered_correctly} questions correctly out of {limit}.")
     else:
-        print(f"You managed to answer correctly {questions_answered} questions in one minute.")
+        print(f"You managed to answer {questions_answered_correctly} questions correctly in one minute.")
 
 
 def get_answer(text):
