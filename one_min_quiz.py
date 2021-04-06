3447823f80c84ec2a86b8864ecd65348dcc25218
AdiDD <adi.deaconu21@gmail.com>
2021-04-06 20:14:31 +0300

    Implement one minute quiz

---
+++ one_min_quiz.py
@@ -0,0 +1,33 @@
+import time
+import random
+
+import util
+import view
+from questions import questions
+
+
+def one_min_quiz():
+    mistakes = 0
+    questions_answered_correctly = 0
+    TIME_LIMIT = 60  # 1 minute
+    START_TIME = time.time()
+
+    while time.time() - START_TIME < TIME_LIMIT:
+        question = random.choice(questions)
+        questions.remove(question)
+        util.clear_terminal()
+        view.print_stats(questions_answered_correctly, mistakes)
+        view.print_question(question)
+        answer = view.get_answer("Please select your answer: ").upper()
+        while answer not in ["A", "B", "C", "D"]:
+            answer = view.get_answer("Pleas enter A, B, C or D: ").upper()
+
+        if answer == question[-1] and time.time() - START_TIME < TIME_LIMIT:
+            questions_answered_correctly += 1
+        elif time.time() - START_TIME < TIME_LIMIT:
+            mistakes += 1
+        else:
+            break
+
+    util.clear_terminal()
+    view.print_finish_screen(questions_answered_correctly, mistakes)
