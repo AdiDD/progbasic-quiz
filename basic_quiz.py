7f21ee6035ca7d3d8ac2d6747b7724a7de807d73
AdiDD <adi.deaconu21@gmail.com>
2021-04-06 23:47:48 +0300

    Add KeyboardInterrupt exception and fix print_finish_screen function call

--- basic_quiz.py
+++ basic_quiz.py
@@ -1,5 +1,6 @@
 import time
 import random
+import sys
 
 import util
 import view
@@ -11,28 +12,30 @@ def basic_quiz():
     questions_answered_correctly = 0
     TIME_LIMIT = 25 * 60  # 25 minutes
     START_TIME = time.time()
+    try:
+        while questions_answered_correctly < 26 and mistakes < 5 and time.time() - START_TIME < TIME_LIMIT:
+            question = random.choice(questions)
+            questions.remove(question)
+            util.clear_terminal()
+            time_left = util.convert_time(time.time() - START_TIME, 20 * 60)
+            view.print_stats(questions_answered_correctly, mistakes, time_left, 25)
+            view.print_question(question)
+            answer = view.get_answer("Please select your answer: ").upper()
+            while answer not in ["A", "B", "C", "D"]:
+                answer = view.get_answer("Pleas enter A, B, C or D: ").upper()
 
-    while questions_answered_correctly < 26 and mistakes < 5 and time.time() - START_TIME < TIME_LIMIT:
-        question = random.choice(questions)
-        questions.remove(question)
+            if util.is_correct(answer, question) and time.time() - START_TIME < TIME_LIMIT:
+                questions_answered_correctly += 1
+            elif time.time() - START_TIME < TIME_LIMIT:
+                mistakes += 1
+            else:
+                break
+    except KeyboardInterrupt:
         util.clear_terminal()
-        time_left = util.convert_time(time.time() - START_TIME, 20 * 60)
-        view.print_stats(questions_answered_correctly, mistakes, time_left, 25)
-        view.print_question(question)
-        answer = view.get_answer("Please select your answer: ").upper()
-        while answer not in ["A", "B", "C", "D"]:
-            answer = view.get_answer("Pleas enter A, B, C or D: ").upper()
-
-        if util.is_correct(answer, question) and time.time() - START_TIME < TIME_LIMIT:
-            questions_answered_correctly += 1
-        elif time.time() - START_TIME < TIME_LIMIT:
-            questions_answered_correctly += 1
-            mistakes += 1
-        else:
-            break
+        sys.exit("Program closed")
 
     util.clear_terminal()
     if questions_answered_correctly > 20:
-        view.print_stats(questions_answered_correctly, mistakes, True, 25)
+        view.print_finish_screen(questions_answered_correctly, True, 25)
     else:
-        view.print_stats(questions_answered_correctly, mistakes, False, 25)
+        view.print_finish_screen(questions_answered_correctly, False, 25)
