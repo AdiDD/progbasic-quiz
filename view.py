a8a269429c7e782af66573eebe75b079734f6299
AdiDD <adi.deaconu21@gmail.com>
2021-04-06 18:57:00 +0300

    Implement print_question, print_stats and print_finish_screen functions

--- view.py
+++ view.py
@@ -1,3 +1,30 @@
 def start_screen():
     print("Welcome to the progbasiz quiz!")
-    input("Press any key to start...")
+    print("If you want to take a basic quiz (25 questions in 20 minutes), press 1")
+    print("If you want to test how many questions you can answer in 1 minute, press 2")
+
+
+def print_question(question):
+    print(question[0])
+    print(question[1])
+    print(question[2])
+    print(question[3])
+    print(question[4])
+
+
+def print_stats(questions_answered, mistakes, limit=0):
+    if limit == 0:
+        print(f"Questions answered: {questions_answered}, Mistakes: {mistakes}")
+    else:
+        print(f"Questions answered: {questions_answered}/{limit}, Mistakes: {mistakes}")
+    print("\n--------------------------------------------------\n")
+
+
+def print_finish_screen(questions_answered, mistakes, passed=True, limit=0):
+    if passed:
+        print("You passed")
+        print(f"Questions answered: {questions_answered}/, Mistakes: {mistakes}")
+
+
+def get_answer(text):
+    return input(text)
