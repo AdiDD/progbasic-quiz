25d3be0a85ad985355d5429fde6e96118ba18fe1
AdiDD <adi.deaconu21@gmail.com>
2021-04-06 20:17:25 +0300

    Create questions.py with 2 provisionary questions for testing

---
+++ questions.py
@@ -0,0 +1,13 @@
+questions = [["Which of the following objects are unordered?",
+              "A. Lists",
+              "B. Strings",
+              "C. Sets",
+              "D. Tuples",
+              "C"
+              ],
+             ["What is the difference between a list and a set?",
+              "A. A list allows duplicate values and a set does not.",
+              "B. A set allows duplicate values and a list does not.",
+              "C. List allow duplicate values and are ordered whereas sets are unordered and do not allow duplicate values.",
+              "D. There are no differences",
+              "C"]]
