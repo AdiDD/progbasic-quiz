11b7617c667222c7a3bcf9fd47bbede4599e3529
AdiDD <adi.deaconu21@gmail.com>
2021-04-06 23:48:37 +0300

    Add Keyboard interrupt exception

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import sys
+from util import clear_terminal
 from view import start_screen, get_answer
-from one_min_quiz import one_min_quiz
+from custom_quiz import custom_quiz
 from basic_quiz import basic_quiz
 
 
 def main():
-    start_screen()
-    answer = get_answer("Make your choice: ")
-    while answer not in ["1", "2"]:
-        answer = get_answer("Pleas enter 1 or 2: ")
-    if answer == "1":
-        basic_quiz()
-    else:
-        one_min_quiz()
-
+    try:
+        start_screen()
+        answer = get_answer("Make your choice: ")
+        while answer not in ["1", "2"]:
+            answer = get_answer("Pleas enter 1 or 2: ")
+        if answer == "1":
+            basic_quiz()
+        else:
+            minutes = get_answer("How many minutes would you like? (max 5) ")
+            while minutes not in ["1", "2", "3", "4", "5"]:
+                minutes = get_answer("Please enter a number between 1 and 5: ")
+            custom_quiz(int(minutes))
+    except KeyboardInterrupt:
+        clear_terminal()
+        sys.exit("Program closed")
 
 if __name__ == "__main__":
     main()
